2c4154852a4b315b57577b9dedbb264acb8db6d2
bevonbrye <bryebevon@gmail.com>
2021-03-25 17:44:03 -0700

    finish drumKit functionality

--- Day1DrumKit/js/main.js
+++ Day1DrumKit/js/main.js
@@ -3,7 +3,10 @@
 
 //doc and screen are two properties for window
 //can be transposed to jQuery
-window.addEventListener('keydown', function(e) { 
+window.addEventListener('keydown', playSound);
+
+//Functions: 
+function playSound(e) { 
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
@@ -11,4 +14,15 @@ window.addEventListener('keydown', function(e) {
     audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
-});
+
+
+}
+
+function removeTransition(e){ 
+    if(e.propertyName !== 'transform') return;
+
+    this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition) )
