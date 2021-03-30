835ad77e313d6bb2cd5c0c373823d231364f38f6
bevonbrye <bryebevon@gmail.com>
2021-03-29 23:39:46 -0700

    finish clock functionality

--- Day2Clock/js/main.js
+++ Day2Clock/js/main.js
@@ -1,5 +1,6 @@
 const secondHand = document.querySelector('.second-hand')
 const minHand = document.querySelector('.min-hand')
+const hourHand = document.querySelector('.hour-hand')
 // function: 
 
 function setDate(){ 
@@ -9,6 +10,14 @@ function setDate(){
     const secondsDegrees = ((seconds / 60) * 360) + 90; 
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`
 
+
+    const mins = now.getMinutes(); 
+    const minDegrees = ((mins / 60) * 360) + 90;
+    minHand.style.transform = `rotate(${minDegrees}deg)`
+
+    const hour = now.getHour(); 
+    const houDegrees = ((mins / 60) * 360) + 90;
+    minHand.style.transform = `rotate(${minDegrees}deg)`
 
 }
 
