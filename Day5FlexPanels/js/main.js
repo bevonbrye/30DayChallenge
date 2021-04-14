3adb2b01d2574f9074ac9386f3011f2cc9f613b3
bevonbrye <bryebevon@gmail.com>
2021-04-14 15:55:46 -0700

    finish Flex Panels

--- Day5FlexPanels/js/main.js
+++ Day5FlexPanels/js/main.js
@@ -1,1 +1,16 @@
 console.log('you are the greatest')
+
+const panels = document.querySelectorAll('.panel'); 
+
+function toggleOpen(){
+    this.classList.toggle('open')
+}
+
+function toggleActive(e) {
+    if (e.propertyName.includes('flex')) {
+      this.classList.toggle('open-active');
+    }
+  }
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitioned', toggleActive));
+
