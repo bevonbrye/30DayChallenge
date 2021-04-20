4341c2202de3dfb10ec5d09cc4752eafb21a3ee3
bevonbrye <bryebevon@gmail.com>
2021-04-20 12:00:45 -0700

    add to cadio two find

--- Day7ArrayCardio2/js/main.js
+++ Day7ArrayCardio2/js/main.js
@@ -22,14 +22,23 @@ const people = [
     
   })
 
-console.log(isAdult)
-
   // Array.prototype.every() // is everyone 19 or older?
+
+    const isEveryOneAdult = people.every(everyPerson => { 
+        const currentYear = (new Date().getFullYear())
+        return currentYear - everyPerson.year >= 19;
+    });
+
+console.log(isEveryOneAdult)
+    
 
   // Array.prototype.find()
   // Find is like filter, but instead returns just the one you are looking for
   // find the comment with the ID of 823423
-
+const findId = comments.find(comment => comment.id === 823423)
+console.log(findId)
   // Array.prototype.findIndex()
   // Find the comment with this ID
   // delete the comment with the ID of 823423
+
+  
